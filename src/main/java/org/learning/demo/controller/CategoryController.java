213900cf53083050f1141e3c3f1917e32a275d5e
hedys9 <hedysadeghi@gmail.com>
2024-01-19 16:52:17 +0100

    fixed

--- src/main/java/org/learning/demo/controller/CategoryController.java
+++ src/main/java/org/learning/demo/controller/CategoryController.java
@@ -6,6 +6,7 @@ import org.learning.demo.repository.CocktailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class CategoryController {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @GetMapping
     public String list(Model model) {
         List<Category> categoryList = categoryRepository.findAll();
         model.addAttribute("categoryList", categoryList);
