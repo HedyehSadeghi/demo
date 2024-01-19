d93a3b0aa1e5589778094449a8ee950930a68188
hedys9 <hedysadeghi@gmail.com>
2024-01-19 16:51:32 +0100

    category repository and controller repository (get mapping "/categories")

---
+++ src/main/java/org/learning/demo/controller/CategoryController.java
@@ -0,0 +1,30 @@
+package org.learning.demo.controller;
+
+import org.learning.demo.model.Category;
+import org.learning.demo.repository.CategoryRepository;
+import org.learning.demo.repository.CocktailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    private CocktailRepository cocktailRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public String list(Model model) {
+        List<Category> categoryList = categoryRepository.findAll();
+        model.addAttribute("categoryList", categoryList);
+        return "categories/list";
+
+    }
+
+
+}
