960bb84a5069a80a5f3b029acb9861e300f46493
hedys9 <hedysadeghi@gmail.com>
2024-01-19 17:15:20 +0100

    list in cocktail controller

--- src/main/java/org/learning/demo/controller/CocktailController.java
+++ src/main/java/org/learning/demo/controller/CocktailController.java
@@ -1,4 +1,34 @@
 package org.learning.demo.controller;
 
+import org.learning.demo.model.Cocktail;
+import org.learning.demo.repository.CategoryRepository;
+import org.learning.demo.repository.CocktailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/cocktails")
 public class CocktailController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private CocktailRepository cocktailRepository;
+
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Cocktail> cocktailList = cocktailRepository.findAll();
+        model.addAttribute("cocktailList", cocktailList);
+        return "cocktails/list";
+
+    }
+
+
 }
