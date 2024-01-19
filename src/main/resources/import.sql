1f57a3b9f3a8abe8038fc71d5fb37294c486bc9e
AlfredoM06 <Alfredomanfredi06@gmail.com>
2024-01-19 17:02:42 +0100

    file sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO db_cocktail.cocktails (description, image, name, price) VALUES('è un cocktail da aperitivo alcolico dal tipico colore rosso chiaro, a base di vermut rosso, bitter Campari e gin.', 'https://www.donnamoderna.com/content/uploads/2023/05/d2e8d64598effae8-scaled.jpg', 'Negroni', 7);
+INSERT INTO db_cocktail.cocktails (description, image, name, price) VALUES('Long drink dolce e dissetante che si prepara con rum bianco, latte di cocco e succo di ananas', 'https://www.giallozafferano.it/images/6-630/Pina-colada_650x433_wm.jpg', 'Pina Colada', 6.5);
+INSERT INTO db_cocktail.cocktails (description, image, name, price) VALUES('Bourbon o con Rye Whiskey, una zolletta di zucchero sciolta nel bicchiere, due gocce di angostura, poca soda e del ghiaccio', 'https://static.cookist.it/wp-content/uploads/sites/21/2018/08/old-fashioned.jpg', 'Old Fashioned', 6);
+
+
+INSERT INTO db_cocktail.categories (description, name) VALUES('Cocktail adatti ai maggiorenni', 'Alcolici');
+INSERT INTO db_cocktail.categories (description, name) VALUES('Cocktail adatti alle feste', 'Aperitivi');
+INSERT INTO db_cocktail.categories (description, name) VALUES('Cocktail adatti ai minori', 'Analcolici');
